d2a71b3caeadd71ed8618e120afb6817c15907a2
Bartosz Majsak <bartosz.majsak@gmail.com>
2016-09-12 23:32:29 +0200

    Implemented TableResource

--- 01_spring_boot/src/main/kotlin/ch/open/chef/orders/Application.kt
+++ 01_spring_boot/src/main/kotlin/ch/open/chef/orders/Application.kt
@@ -2,6 +2,8 @@ package ch.open.chef.orders
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 open class Application {
@@ -10,5 +12,13 @@ open class Application {
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
+}
+
+@RestController
+open class TableResource {
+
+    @RequestMapping("/tables")
+    fun allTables() = arrayOf(1, 2, 3)
+
 }
 
